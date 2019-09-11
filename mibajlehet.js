5a4bfb671f9781ac1bf4d2b2ed895b7d561b6cfb
Attila Gyöngyösi <attilagyongyosi@users.noreply.github.com>
2019-09-12 00:32:14 +0200

    Update mibajlehet.js

--- mibajlehet.js
+++ mibajlehet.js
@@ -1,9 +1,13 @@
-const problems = [
-    'Hanufer Leánybúcsú'
-];
+const drinks = [	const problems = [
+    'semmi',	    'EU Staging Downsizing'
+    'nem sok',	
+    'kérdezd babót',	
+    'hozz egy sört'	
+];	];
 
-const randomProblem = problems[ Math.floor(Math.random() * problems.length) ];
 
-window.onload = function () {
-    document.getElementById('mibajlehet').innerText = randomProblem;
+const randomDrink = drinks[ Math.floor(Math.random() * drinks.length) ];	const randomProblem = problems[ Math.floor(Math.random() * problems.length) ];
+
+window.onload = function () {	window.onload = function () {
+    document.getElementById('mibajlehet').innerText = randomDrink;	    document.getElementById('mibajlehet').innerText = randomProblem;
 }
