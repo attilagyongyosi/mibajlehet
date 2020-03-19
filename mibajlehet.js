3c95a5dad3194de53efffc9840d128bd82cf6df0
attilagyongyosi <attilagyongyosi0@gmail.com>
2020-03-19 19:06:11 +0100

    Korona edition 2

--- mibajlehet.js
+++ mibajlehet.js
@@ -1,5 +1,6 @@
 const PROBLEMS = [
-    'Bezár az Ibolya a koronavírus miatt'
+    'Bezár az Ibolya a koronavírus miatt',
+    'Bangladesben tömeges imádkozást szerveznek a koronavírus ellen'
 ];
 
 const randomProblem = PROBLEMS[ Math.floor(Math.random() * PROBLEMS.length) ];
