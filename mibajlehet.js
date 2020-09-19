5828deed6a77510e2b1e2a9f1fc58cdc70603a55
Attila Gyöngyösi <attilagyongyosi@users.noreply.github.com>
2020-09-19 18:09:41 +0200

    Update mibajlehet.js

--- mibajlehet.js
+++ mibajlehet.js
@@ -1,5 +1,5 @@
 const PROBLEMS = [
-    'PingFederate 10 upgrade az univerzum legnagyobb Navigator regressionje közben'
+    'Alíz néni az Ibolyában'
 ];
 
 const randomProblem = PROBLEMS[ Math.floor(Math.random() * PROBLEMS.length) ];
